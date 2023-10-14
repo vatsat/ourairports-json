a43c39c76527fcc1f08eaca7b8cb51e786be2bbd
Mr. K V <36492979+69u@users.noreply.github.com>
2023-10-14 21:31:08 +0800

    yewww

---
+++ activeAirports.js
@@ -0,0 +1,62 @@
+const fs = require('node:fs');
+
+fs.readFile('airports.json', async function(err, data) {
+    if (err) {
+        return console.error(err)
+    }
+    const airports = await JSON.parse(data);
+    await matrix(airports)
+
+});
+
+async function matrix(airports){
+    const activeAirports = {};
+    const vatsimPilots = await fetch(
+        "https://vatsat.pages.dev/api/main?source=vatsimPilots",
+    ).then((response) => response.json());
+    vatsimPilots.source.data.features.forEach((pilot) => {
+        if (pilot.properties.flightPlan) {
+          const flightPlan = JSON.parse(pilot.properties.flightPlan);
+          if (activeAirports[flightPlan.departure]) {
+            activeAirports[flightPlan.departure].departures++;
+          } else if (!activeAirports[flightPlan.departure]){
+            activeAirports[flightPlan.departure] = {
+              departures: 0,
+              arrivals: 0,
+            };
+          }
+  
+          if (activeAirports[flightPlan.arrival]) {
+            activeAirports[flightPlan.arrival].arrivals++;
+          } else if (!activeAirports[flightPlan.arrival]){
+            activeAirports[flightPlan.arrival] = {
+              arrivals: 0,
+              departures: 0,
+            };
+          }
+        }
+      });
+      Object.keys(activeAirports).forEach(icao => {
+        if(activeAirports[icao].departures === 0 && activeAirports[icao].arrivals === 0) {
+            delete activeAirports[icao];
+        }
+      })
+      airports.forEach((airport) => {
+        if (
+          Object.keys(activeAirports).filter(
+            (airportICAO) => airportICAO === airport.gps_code,
+          )[0]
+        ) {
+          Object.assign(activeAirports[airport.gps_code], airport);
+        }
+      });
+
+      await writeFile('activeAirports.json', JSON.stringify(activeAirports))
+}
+
+async function writeFile(path, data){
+    fs.writeFile(path, data, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+}
