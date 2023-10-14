8fccc2aa951cd52590136d247e9cd613a08bfbc8
Mr. K V <36492979+69u@users.noreply.github.com>
2023-10-14 12:34:33 +0800

    fixed converter

--- convert.js
+++ convert.js
@@ -3,14 +3,32 @@ const fs = require('fs');
 
 const fileInputName = 'airports.csv'; 
 const fileOutputName = 'airports.json';
+let jsonString = '';
 
-let json = csvToJson.fieldDelimiter(',').supportQuotedField(true).getJsonFromCsv(fileInputName);
-let jsonString = JSON.stringify(json)
+convertCSVtoJSON().then(saveJSON);
 
-fs.writeFile(`${fileOutputName}`, jsonString, err => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log(`Successfully wrote file: ${fileOutputName}`)
-    }
-});
+async function convertCSVtoJSON(){
+    let json = await csvToJson.fieldDelimiter(',').supportQuotedField(true).getJsonFromCsv(fileInputName);
+    let updatedJson = []
+    await json.forEach((airport, index) => {
+        let tempJson = {};
+        Object.keys(airport).forEach(key => {
+            let name = key.substring(1, key.length - 1)
+            tempJson[name] = airport[key]
+            delete json[key];
+        })
+        updatedJson.push(tempJson)
+    })
+    console.log(updatedJson)
+    jsonString = JSON.stringify(updatedJson)
+}
+
+function saveJSON(){
+    fs.writeFile(`${fileOutputName}`, jsonString, err => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(`Successfully wrote file: ${fileOutputName}`)
+        }
+    });
+}
